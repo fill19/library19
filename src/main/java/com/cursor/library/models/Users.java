c9b0283d3252659d15c050fc3c71efca1b4de556
Dmytro <65898747+fill19@users.noreply.github.com>
2021-02-21 17:39:04 +0200

    Add files via upload

---
+++ src/main/java/com/cursor/library/models/Users.java
@@ -0,0 +1,85 @@
+package com.cursor.library.models;
+
+import com.cursor.library.daos.UserPermission;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "users")
+public class Users implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String userName;
+    private String password;
+    @ElementCollection(targetClass = UserPermission.class, fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<UserPermission> permissions;
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPermissions(Set<UserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Set<UserPermission> getPermissions() {
+        return permissions;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return permissions.stream().map(r -> new SimpleGrantedAuthority(r.name())).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
